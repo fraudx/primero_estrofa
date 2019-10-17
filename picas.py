880fa3057bd642291600416aa47a904d045e029e
fraudx <56617452+fraudx@users.noreply.github.com>
2019-10-16 21:35:47 -0500

    v2

--- picas.py
+++ picas.py
@@ -24,12 +24,23 @@ while True:
 
 print(usuario, numero)
 
-if (usuario) == (numero):
-     print ("el numero es igual")
 
+#recorrer listas
 
-            
-        
+fijas = 0
+picas = 0
+for i in range(len(numero)):
+    if numero[i]== usuario[i]:
+        fijas += 1
 
+for i in range(len(numero)):
+    if j in range(len(usuario)):
+        if numero[i] == usuario[j] and i != j:
+                  picas += 1
 
-
+                
+print("fijas = " + str(fijas))
+print("picas = " + str(picas))
+if fijas == cantidad:
+    Break
+                  
